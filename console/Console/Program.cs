f3693157a15e67a14c0701ccccdffc24617b65fd
Aleksandr <kubartphil@gmail.com>
2021-11-21 21:09:54 +0300

    Test push

---
+++ console/Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = 10; 
+int y = x + 10;
+
+Console.WriteLine("Hello, This me in C#");
+Console.WriteLine("Hello, World!");
+
+
